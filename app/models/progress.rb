0d5ef4a7e5e81b3f21e9d7d78d4653fad17e0778
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-02 14:02:25 +0100

    validate the attributes of the Progress model

--- app/models/progress.rb
+++ app/models/progress.rb
@@ -1,4 +1,9 @@
 class Progress < ApplicationRecord
+
   belongs_to :user
   belongs_to :lesson
+
+  # Validations
+  validates :user_id, uniqueness: { scope: :lesson_id, message: 'can only track progress once per lesson' }
+  validates :completed, inclusion: { in: [true, false] }
 end
