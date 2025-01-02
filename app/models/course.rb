643e75b31513b4344ddb3ebc5831c04bc1aee960
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-02 13:51:38 +0100

    Add the difficulty level to the course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,8 @@
 class Course < ApplicationRecord
+
     has_many :lessons, dependent: :destroy
+    
     validates :title, presence: true
-    validates :description, presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+  validates :difficulty_level, presence: true, inclusion: { in: %w[beginner intermediate advanced] }
 end
