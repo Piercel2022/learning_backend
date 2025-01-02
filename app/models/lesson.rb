4cdb1dc8aff2996179d91f7a13781860e0df6780
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-02 13:56:35 +0100

    validate the title in the Lesson model

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,4 +5,6 @@ class Lesson < ApplicationRecord
   belongs_to :course
   has_many :progresses, dependent: :destroy
   has_many :users, through: :progresses
+
+  validates :title, presence: true, uniqueness: { scope: :course_id }
 end
