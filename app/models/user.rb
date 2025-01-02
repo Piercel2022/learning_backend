07218719b8b555f471f164f341fc2155d94fd2b0
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2025-01-02 13:43:30 +0100

    Validate the role in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
+  validates :role, inclusion: { in: ROLES }
 end
